8979aeb887ccc500771957f2e4995efba42df08a
ivankp <ivan.pogrebnyak@gmail.com>
2021-07-30 21:45:26 -0400

    merge groups

--- merge.py
+++ merge.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from lxml import etree
 import re, json
+from glob import glob
+from lxml import etree
 
 games = [
     ['id','year',
@@ -19,45 +20,47 @@ def try_convert(x,f,x0=None):
     except:
         return x0
 
-for fi in range(1,9000):
-    g = etree.parse(f'bgg/{fi}.xml')
-    g = g.xpath('/boardgames/boardgame')[0]
-    x = g.get('objectid')
-    if x is None:
-        continue
-    game = [ int(x) ]
+def split_ints(s):
+    return [ int(x) if i%2 else x for i,x in enumerate(re.split(r'(\d+)',s)) ]
 
-    game.append( try_convert(g.xpath('yearpublished')[0].text,int) )
-    game.append( try_convert(g.xpath('minplayers')[0].text,int) )
-    game.append( try_convert(g.xpath('maxplayers')[0].text,int) )
-    game.append( try_convert(g.xpath('playingtime')[0].text,float) )
-    game.append( try_convert(g.xpath('minplaytime')[0].text,float) )
-    game.append( try_convert(g.xpath('maxplaytime')[0].text,float) )
-    game.append( try_convert(g.xpath('age')[0].text,float) )
+for fname in sorted(glob('bgg/*.xml'),key=split_ints):
+    print(fname)
+    for g in etree.parse(fname).xpath('/boardgames/boardgame'):
+        x = g.get('objectid')
+        if x is None:
+            continue
+        game = [ int(x) ]
 
-    game.append( [ x.text for x in g.xpath('boardgamemechanic') ] )
-    game.append( [ x.text for x in g.xpath('boardgamecategory') ] )
-    game.append( [ x.text for x in g.xpath('boardgamefamily') ] )
-    game.append( len(g.xpath('boardgameexpansion')) )
+        game.append( try_convert(g.xpath('yearpublished')[0].text,int) )
+        game.append( try_convert(g.xpath('minplayers')[0].text,int) )
+        game.append( try_convert(g.xpath('maxplayers')[0].text,int) )
+        game.append( try_convert(g.xpath('playingtime')[0].text,float) )
+        game.append( try_convert(g.xpath('minplaytime')[0].text,float) )
+        game.append( try_convert(g.xpath('maxplaytime')[0].text,float) )
+        game.append( try_convert(g.xpath('age')[0].text,float) )
 
-    game.append( [ [
-        int(re.search(r'\d+',x.get('value')).group()),
-        int(x.get('numvotes'))
-    ] for x in g.xpath(
-        'poll[@name="suggested_playerage"]/results/result'
-    ) ] )
+        game.append( [ x.text for x in g.xpath('boardgamemechanic') ] )
+        game.append( [ x.text for x in g.xpath('boardgamecategory') ] )
+        game.append( [ x.text for x in g.xpath('boardgamefamily') ] )
+        game.append( len(g.xpath('boardgameexpansion')) )
 
-    game.append( [ [
-        int(x.xpath(f'result[@value="{v}"]')[0].get('numvotes'))
-        for v in ['Best','Recommended','Not Recommended']
-    ] for x in g.xpath(
-        'poll[@name="suggested_numplayers"]/results'
-    ) ] )
+        game.append( [ [
+            int(re.search(r'\d+',x.get('value')).group()),
+            int(x.get('numvotes'))
+        ] for x in g.xpath(
+            'poll[@name="suggested_playerage"]/results/result'
+        ) ] )
 
-    games.append(game)
+        game.append( [ [
+            int(x.xpath(f'result[@value="{v}"]')[0].get('numvotes'))
+            for v in ['Best','Recommended','Not Recommended']
+        ] for x in g.xpath(
+            'poll[@name="suggested_numplayers"]/results'
+        ) ] )
 
-# for g in games:
-#     print(g)
+        games.append(game)
+
+print(f'num games: {len(games)-1}')
 
 with open('bgg_data.json','w') as f:
     json.dump(games,f,separators=(',',':'))
