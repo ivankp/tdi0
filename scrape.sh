fc4bb02163f694e84f498c7fdb8315b0ab11eda6
ivankp <ivan.pogrebnyak@gmail.com>
2021-07-30 21:19:01 -0400

    scrape in groups of 100

--- scrape.sh
+++ scrape.sh
@@ -3,11 +3,11 @@
 
 mkdir -p bgg
 
-for i in {1..10000}; do
+for ((i=0; i<200000; i+=100)); do
   echo $i
   f="bgg/${i}.xml"
   if [ ! -f "$f" ]; then
-    curl -L "http://www.boardgamegeek.com/xmlapi/boardgame/${i}" \
+    curl -L "http://www.boardgamegeek.com/xmlapi/boardgame/$(seq -s, $i $((i+99)))?stats=1" \
     | sed 's/\s\+$//;/^$/d' \
     > "$f"
   fi
